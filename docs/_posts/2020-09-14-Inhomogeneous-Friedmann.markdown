dd089d1faea70cef541e8c525a6cc0ca39f30059
Tom Abel <tabel@stanford.edu>
2020-09-17 17:30:48 -0700

    inhomogeneous updates

--- docs/_posts/2020-09-14-Inhomogeneous-Friedmann.markdown
+++ docs/_posts/2020-09-14-Inhomogeneous-Friedmann.markdown
@@ -19,8 +19,39 @@ and that using $\rho_{crit,0}= {3H_0^2 \over 8\pi G}$ and $H=\dot{a}/a$ we could
 
 $$H(a)^2= \Omega_{m,0}\,a^{-3} - \Omega_{k,0}\,a^{-2} + \Omega_\Lambda,
 $$
+
 where $\Omega_k= k c^2/R_c^2/\rho_{crit,0}$ and $\Omega_\Lambda = \Lambda c^2/3/\rho_{crit,0}$. One important thing to note is that all the $\Omega$s have to add up to 1. This is true at all times but we wrote the equation here so that we give all the $\Omega$s only at the time when $a=1$ and keep their (general versions) $a$ dependence explicit in the equation. This is indicated by using the subscript $0$ in $\Omega_{m,0}$. The zero refers to the cosmological redshift $1+z\equiv 1/a$ which is often used as an  way to denote which epoch of the Universe one is referring to. For a fixed cosmological model either of $a$, $z$, or time are used for this depending on what one is interesting to highlight. How much the light redshifts from that period to now, how old the Universe was or how much smaller a specific distance would have been at that time.
 
 All $\Omega$s have to add to 1 by definition. In an analogy to the Newtonian equivalent equation the Friedmann equation is derived from the Energy condition. The $k$ denotes there whether the total energy is exactly zero, negative for a gravitational bound objects or positive for which the total energy would be positive.
 
-Consider a flat background Universe with matter density $\rho_b$ but assuming its matter density, $\rho_i$ that is slightly different. We define a density contrast $\delta_i=\rho_i/\rho_b-1$.
+
+At the current time when $a=1$ and $\dot{a}=H_0$, $\Omega_{k,0} = 1 - \Omega_{m,0} - \Omega_{\Lambda,0}$, can be thought of as the amount of matter missing that would have made the Universe closed. If $\Omega_{k,0}<0$ then we have a Universe that is closed and will collapse eventually. 
+
+If we consider these density parameters at an earlier time, say at $z=99$ where $a=0.01$ they would take very different numerical values because the $\rho_{crit}$ which they are measured against depends on $H(z)^2$ 
+
+Consider a Universe with matter density $\rho$ that has a matter density slightly different than a flat universe with the same cosmological constant and matter density $\rho_b$. Write this difference as a density contrast $\delta=\rho/\rho_b-1$.
+This Universe now has a curvature that depends on $\delta$. So we would have 
+
+$$\frac{\dot{a}^2}{a^2} = \frac{8 \pi G \rho}{3} (1+\delta) -  \frac{kc^2}{a^2R_c^2} + \frac{\Lambda c^2}{3}, $$
+
+and if we assume that for a very small delta at very high redshift $z_i$ (small $a$) it expands as fast as the background Universe for which,
+
+$$
+\frac{\dot{a}^2}{a^2} = \frac{8 \pi G \rho}{3} + \frac{\Lambda c^2}{3}, 
+$$
+
+we can set these two equations equal and see that,
+
+$$\frac{8 \pi G \rho}{3} \delta =  \frac{kc^2}{a^2R_c^2}. $$ 
+
+In terms of density parameters that tells us that the curvature corresponding to a slight density perturbation $\delta$ at redshift $z_i$ is given by:
+
+$$\Omega_{k,0} = (1+z_i)\Omega_{m,0}\delta .$$
+
+To derive this we assumed that the expansion rate in our perturbed Universe is exactly the same as the one of the unperturbed. If one includes that the Hubble rate at $z_i$ where we specify $\delta$ is perturbed so that $H(z_i)=H_b(z_i)(1-\delta)$ one finds the equivalent $\Omega_{k,0}$. 
+$$\Omega_{k,0} = {\delta \left[ a_i^3(\delta-6)(\Omega_m-1)- (\delta-15)\Omega_m \right] \over 9 a_i}
+  \approx \frac{5}{3}(1+z_i)\Omega_{m,0}\delta .$$
+
+The last approximation here is very accurate as all other terms have at least order $a_i^2$ or $\delta^2$ or even smaller. 
+So we can now compare now how different the scale factors evolve in the flat background Universe as compared to the one corresponding to the one with the slight early difference in density. 
+
---
+++ docs/posts_code/InhomogeneousFriedmannEasy.py
@@ -0,0 +1,286 @@
+# %% 
+import numpy as np
+from scipy.integrate import odeint
+from scipy.integrate import quad
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+plt.style.use('./Modified5308.mplstyle') 
+cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+figtransparent = 0
+
+class cosmo:
+    """Keep parameters for a Universe with curvature
+    H0 = Hubble constant today: in km/s/Mpc
+    Om0: Omega_matter at current time
+    Ok0: Omega_curvature today set to 1-Omega_matter-Omega_Lambda
+    OL0: Omega_lambda 
+     """
+    def __init__(self,params):
+        self.H0 = params['H0']
+        self.Om0 = params['Om0']
+        self.OL0 = params['OL0']
+        self.Ok0 = 1.0 - self.Om0 - self.OL0
+        
+    def hubble(self,a):
+        return np.sqrt(self.H0**2*(self.Om0/a**3-self.Ok0/a**2+(1-self.Om0)))
+
+    def hubble_from_z(self,z):
+        return hubble(self,1/(1+z))
+
+params = {'H0':67.11, 'Om0':0.3175, 'OL0':0.6825}
+
+u1 = cosmo( {'H0':67.11, 'Om0':0.3175, 'OL0':0.6825})
+u2 = cosmo( {'H0':67.11, 'Om0':0.3175, 'OL0':0.6825})
+u3 = cosmo( {'H0':67.11, 'Om0':0.28, 'OL0':0.6825})
+u4 = cosmo( {'H0':67.11, 'Om0':0.4, 'OL0':0.6825})
+universe = cosmo(params)
+
+ar = np.logspace(-2,.4,400)
+
+plt.figure(figsize=(10,8))
+plt.loglog(ar,ar*u1.hubble(ar), label=r"$H_0={:.1f},\ \Omega_m$={:.2f}".format(u1.H0,u1.Om0),lw=4)
+plt.loglog(ar,ar*u2.hubble(ar), '--',label=r"$H_0={:.1f},\ \Omega_m$={:.2f}".format(u2.H0,u2.Om0),alpha=.7)
+plt.loglog(ar,ar*u3.hubble(ar), '--',label=r"$H_0={:.1f},\ \Omega_m$={:.2f}".format(u3.H0,u3.Om0),alpha=.7)
+plt.loglog(ar,ar*u4.hubble(ar), '--',label=r"$H_0={:.1f},\ \Omega_m$={:.2f}".format(u4.H0,u4.Om0),alpha=.7)
+
+plt.plot([1],[u1.H0],'o',ms=8,alpha=.75, label="today")
+
+plt.xlim(1e-2,3)
+plt.ylim(1e1,5e2)
+
+plt.legend()
+plt.ylabel(r"$\dot{a}=a\,H(a)$",)
+plt.xlabel(r"$a$")
+plt.title("spatially flat cosmologies")
+#plt.savefig("../assets/inhomogeneous-Hubble-as-function-of-a.png", dpi=150,transparent=figtransparent)
+# %% [markdown]
+# <h2>Integrate</h2>
+#
+
+# %%
+def a_from_t_for_delta(delta, lrhs):
+    a0 = [1e-3] # at a=1 we want da/dt=1 we can later get units from H0.
+    zi = 999
+    t = np.linspace(1e-6,5, 190000)
+
+    ts=t
+    sol = odeint(lrhs, a0, ts, args= (Om, zi, delta) )
+
+    ts = ts[np.isfinite(sol.flatten())]
+    sol = sol[np.isfinite(sol.flatten())].flatten()
+    return ts, interp1d(ts,sol,bounds_error=None, fill_value="extrapolate")
+
+def rhsf(a,t,Om):
+    return np.sqrt(Om/a+(1-Om)*a**2)
+def rhs(a,t, Om, OL0):
+    return np.sqrt(Om/a-(1-Om-OL0) + OL0*a**2)
+def rhs_del(a,t, Om, zi, delta):
+    ai = 1/(1+zi)
+    Ok0 = (1+zi)*Om*delta
+    Ok0 = (delta*(ai**3*(-6 + delta)*(-1 + Om) - (-15 + delta)*Om))/(9.*ai)
+    OL0 = 1-Om-Ok0 # *(1+zi)**3
+ #   bH = rhsf(1/(1+zi), 0, Om)*
+    return np.sqrt(Om/a*(1+delta)-Ok0 + OL0*a**2) 
+
+def rhs_sph(a,t, Om, zi, delta):
+    Ok0 = (1+zi)*Om*delta *5/3
+    OL0 = 1-Om-Ok0 # *(1+zi)**3
+#    bH = rhsf(1/(1+zi), 0, Om)
+    return np.sqrt(Om/a*(1+delta)-Ok0 + OL0*a**2) #/bH
+
+Om = .3
+ts, a_from_t = a_from_t_for_delta(0., rhs_del)
+d1 = 7e-3
+ts, ad1 = a_from_t_for_delta(d1, rhs_del)
+ts, mad1 = a_from_t_for_delta(-d1, rhs_del)
+ts, as1 = a_from_t_for_delta(d1, rhs_sph)
+ts, mas1 = a_from_t_for_delta(-d1, rhs_sph)
+
+plt.plot(1/a_from_t(ts), mad1(ts)/a_from_t(ts)-1,'--',c=cycle[0],label=r"$\delta=-{:.1g}$".format(d1),alpha=.5)
+plt.plot(1/a_from_t(ts), ad1(ts)/a_from_t(ts)-1,c=cycle[0],label=r"$\delta={:.1g}$".format(d1),alpha=.5)
+plt.plot(1/a_from_t(ts), mas1(ts)/a_from_t(ts)-1,'--',c=cycle[1],label=r"$\delta=-{:.1g}$".format(d1),alpha=.5)
+plt.plot(1/a_from_t(ts), as1(ts)/a_from_t(ts)-1,c=cycle[1],label=r"$\delta={:.1g}$".format(d1),alpha=.5)
+
+plt.xscale("log")
+plt.xlim(1000,1)
+plt.ylim(-1.3,1.3)
+plt.xlabel(r"$1+z$")
+plt.ylabel(r"${a_\delta}/{a_b}-1$")
+plt.title(r"change of scale factor vs background $\Omega_m={:.3f}$".format(Om))
+#plt.loglog(1/solb,sol/solb,label=r"$\delta(z=99)=0.01$")
+plt.legend()
+# %%
+def g_flat(ai, Omi):
+    Ol = 1-Omi # flat Universe of only mater and lambda
+    hubble = lambda a:  np.sqrt(Omi/a**3 + Ol)
+    rhs_g = lambda a: 1/a**3/hubble(a)**3
+    gint = quad(rhs_g,0,ai)[0]
+    g_f = 2.5*Omi*hubble(ai)/ai
+    return g_f*gint
+# Fitting formula for flat Universes
+#    Om = Omi/a**3
+#    return (5*Om/(2*Om**(4/7)-Ol+(1+Om/2)*(1+Ol/70)))
+
+g_flat_vec = np.vectorize(g_flat)
+
+a = np.logspace(-2,0,50)
+plt.figure(figsize=(10,8))
+plt.plot(1/a, g_flat_vec(a,1)*a, label=r"$\Omega_m=1, \, \Omega_\Lambda=0$")
+plt.plot(1/a, g_flat_vec(a,0.4)*a, label=r"$\Omega_m=0.4, \, \Omega_\Lambda=0.6$")
+plt.plot(1/a, g_flat_vec(a,0.3)*a, label=r"$\Omega_m=0.3, \, \Omega_\Lambda=0.7$")
+plt.plot(1/a, g_flat_vec(a,0.2)*a, label=r"$\Omega_m=0.2, \, \Omega_\Lambda=0.8$")
+plt.plot(1/a, g_flat_vec(a,0.1)*a, label=r"$\Omega_m=0.1, \, \Omega_\Lambda=0.9$")
+plt.plot(1/a, g_flat_vec(a,0.01)*a, label=r"$\Omega_m=0.01, \, \Omega_\Lambda=0.99$")
+plt.legend()
+plt.xscale("log")
+plt.yscale("log")
+plt.xlim(10,1)
+plt.ylim(1e-1,1)
+plt.xlabel(r"$1+z$")
+#plt.xticks([7,5,3,1], ["7","5","3","1"])
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator)
+plt.gca().xaxis.set_minor_formatter(FormatStrFormatter('%d'))
+plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%d'))
+plt.gca().yaxis.set_minor_formatter(FormatStrFormatter('%.1f'))
+plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+plt.title("growth factor for flat cosmologies")
+plt.show()
+plt.savefig("../assets/growthfactor-inhomogeneous-as-function-of-a.png", dpi=150,transparent=figtransparent)
+
+
+
+#%%
+
+d1 = 5e-4
+d2 = 1e-3
+d3 = 7e-3
+ts, ad1 = a_from_t_for_delta(d1, rhs_del)
+ts, mad1 = a_from_t_for_delta(-d1, rhs_del)
+ts, ad2 = a_from_t_for_delta(d2, rhs_del)
+ts, mad2 = a_from_t_for_delta(-d2, rhs_del)
+ts, ad3 = a_from_t_for_delta(d3, rhs_del)
+ts, mad3 = a_from_t_for_delta(-d3, rhs_del)
+
+plt.plot(1/a_from_t(ts), mad3(ts)/a_from_t(ts)-1,'--',c=cycle[2],label=r"$\delta(z=999)=-{:.1g}$".format(d3))
+plt.plot(1/a_from_t(ts), mad2(ts)/a_from_t(ts)-1,'--',c=cycle[1],label=r"$\delta=-{:.1g}$".format(d2))
+plt.plot(1/a_from_t(ts), mad1(ts)/a_from_t(ts)-1,'--',c=cycle[0],label=r"$\delta=-{:.1g}$".format(d1))
+plt.plot(1/a_from_t(ts), ad1(ts)/a_from_t(ts)-1,c=cycle[0],label=r"$\delta={:.1g}$".format(d1))
+plt.plot(1/a_from_t(ts), -1*(mad1(ts)/a_from_t(ts)-1),'--',c=cycle[0],alpha=.9,lw=1)
+
+plt.plot(1/a_from_t(ts), ad2(ts)/a_from_t(ts)-1,c=cycle[1],label=r"$\delta={:.1g}$".format(d2))
+plt.plot(1/a_from_t(ts), -1*(mad2(ts)/a_from_t(ts)-1),'--',c=cycle[1],alpha=.9,lw=1)
+
+plt.plot(1/a_from_t(ts), ad3(ts)/a_from_t(ts)-1,c=cycle[2],label=r"$\delta={:.1g}$".format(d3))
+plt.plot(1/a_from_t(ts), -1*(mad3(ts)/a_from_t(ts)-1),'--',c=cycle[2],alpha=.9,lw=1)
+
+plt.plot(1/a_from_t(ts), -(d1*a_from_t(ts)/1e-3),'--',c=cycle[0],alpha=.9,lw=1)
+
+
+plt.xscale("log")
+plt.xlim(1000,1)
+plt.ylim(-.3,.3)
+plt.xlabel(r"$1+z$")
+plt.ylabel(r"${a_\delta}/{a_b}-1$")
+plt.title(r"change of scale factor vs background $\Omega_m={:.1f}$".format(Om))#plt.loglog(1/solb,sol/solb,label=r"$\delta(z=99)=0.01$")
+plt.legend()
+
+# %%
+
+#%%
+
+d1 = 5e-4
+d2 = 1e-3
+d3 = 7e-3
+ts, ad1 = a_from_t_for_delta(d1, rhs_del)
+ts, mad1 = a_from_t_for_delta(-d1, rhs_del)
+ts, ad2 = a_from_t_for_delta(d2, rhs_del)
+ts, mad2 = a_from_t_for_delta(-d2, rhs_del)
+ts, ad3 = a_from_t_for_delta(d3, rhs_del)
+ts, mad3 = a_from_t_for_delta(-d3, rhs_del)
+
+plt.plot(1/a_from_t(ts), (mad3(ts)/a_from_t(ts)-1)/d3/1e3,'--',c=cycle[2],label=r"$\delta(z=999)=-{:.1g}$".format(d3))
+plt.plot(1/a_from_t(ts), (mad2(ts)/a_from_t(ts)-1)/d2/1e3,'--',c=cycle[1],label=r"$\delta=-{:.1g}$".format(d2))
+plt.plot(1/a_from_t(ts), (mad1(ts)/a_from_t(ts)/d1-1)/d1/1e3,'--',c=cycle[0],label=r"$\delta=-{:.1g}$".format(d1))
+plt.plot(1/a_from_t(ts), (ad1(ts)/a_from_t(ts)-1)/d1/1e3,c=cycle[0],label=r"$\delta={:.1g}$".format(d1))
+plt.plot(1/a_from_t(ts), -1*(mad1(ts)/a_from_t(ts)-1)/d1/1e3,'--',c=cycle[0],alpha=.9,lw=1)
+
+plt.plot(1/a_from_t(ts), (ad2(ts)/a_from_t(ts)-1)/d2/1e3,c=cycle[1],label=r"$\delta={:.1g}$".format(d2))
+plt.plot(1/a_from_t(ts), -1*(mad2(ts)/a_from_t(ts)-1)/d2/1e3,'--',c=cycle[1],alpha=.9,lw=1)
+
+plt.plot(1/a_from_t(ts), (ad3(ts)/a_from_t(ts)-1)/d3/1e3,c=cycle[2],label=r"$\delta={:.1g}$".format(d3))
+plt.plot(1/a_from_t(ts), -1*(mad3(ts)/a_from_t(ts)-1)/d3/1e3,'--',c=cycle[2],alpha=.9,lw=1)
+
+da = ((1+d1*a_from_t(ts)/a_from_t(0))**(-1/3)-1)/d1/1e3
+plt.plot(1/a_from_t(ts),da*a_from_t(ts), "o", c="grey", markevery=10, ms=6, label=r"growth factor $g(a)$")
+
+
+plt.xscale("log")
+plt.xlim(1000,1)
+plt.ylim(-.5,.5)
+plt.xlabel(r"$1+z$")
+plt.ylabel(r"$({a_\delta}/{a_b}-1)/\delta$")
+plt.title(r"change of scale factor vs background $\Omega_m={:.1f}$".format(Om))#plt.loglog(1/solb,sol/solb,label=r"$\delta(z=99)=0.01$")
+plt.legend()
+
+# %%
+
+
+# %%
+
+delt = np.logspace(-5,-2,20)
+d1 = 5e-4
+d2 = 1e-3
+d3 = 7e-3
+ts, ad1 = a_from_t_for_delta(d1, rhs_del)
+ts, mad1 = a_from_t_for_delta(-d1, rhs_del)
+ts, ad2 = a_from_t_for_delta(d2, rhs_del)
+ts, mad2 = a_from_t_for_delta(-d2, rhs_del)
+ts, ad3 = a_from_t_for_delta(d3, rhs_del)
+ts, mad3 = a_from_t_for_delta(-d3, rhs_del)
+
+plt.plot(1/a_from_t(ts), mad3(ts),'--',c=cycle[2],label=r"$\delta(z=999)=-{:.1g}$".format(d3))
+plt.plot(1/a_from_t(ts), ad3(ts),'--',c=cycle[2],label=r"$\delta(z=999)={:.1g}$".format(d3))
+
+plt.plot(1/a_from_t(ts), mad2(ts),'--',c=cycle[1],label=r"$\delta(z=999)=-{:.1g}$".format(d2))
+plt.plot(1/a_from_t(ts), ad2(ts),'--',c=cycle[1],label=r"$\delta(z=999)={:.1g}$".format(d2))
+
+plt.plot(1/a_from_t(ts), mad2(ts),'--',c=cycle[1],label=r"$\delta(z=999)=-{:.1g}$".format(d2))
+plt.plot(1/a_from_t(ts), ad2(ts),'--',c=cycle[1],label=r"$\delta(z=999)={:.1g}$".format(d2))
+
+plt.xscale("log")
+plt.yscale("log")
+plt.xlim(100,1)
+plt.ylim(1e-2,2)
+plt.xlabel(r"$1+z$")
+plt.ylabel(r"$({a_\delta}/{a_b}-1)/\delta$")
+plt.title(r"change of scale factor vs background $\Omega_m={:.1f}$".format(Om))#plt.loglog(1/solb,sol/solb,label=r"$\delta(z=99)=0.01$")
+plt.legend()
+# %%
+
+a0 = [1e-3] # at a=1 we want da/dt=1 we can later get units from H0.
+zi = 999
+t = np.linspace(1e-6,5, 190000)
+ts=t
+sol = odeint(rhsf, 1e-3, ts, args= (Om, ) )
+ts = ts[np.isfinite(sol.flatten())]
+sol = sol[np.isfinite(sol.flatten())].flatten()
+
+t_from_a = interp1d(sol,ts,bounds_error=None,fill_value="extrapolate")
+
+ctime = t_from_a(1)
+ft = np.array([1e-6,ctime])
+delt = -1*np.logspace(-7,-3,100)
+
+fdel = np.array([])
+for delta in delt:
+    sol = odeint(rhs_del, 1e-3, ft, args= (Om, 999, delta))
+#    print(delta, sol[1]/(delta+1)**(-1/3))
+    fdel = np.append(fdel,sol[1])
+
+plt.plot(-delt*1e3,-(fdel**3-1)/delt/1e3)
+plt.xscale("log")
+#plt.yscale("log")
+
+# %%
